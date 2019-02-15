a64a1469b5f3d61f39486180e9c2bc684c563e50
Joanna Szylo <joannaszylo.com>
2019-02-15 23:22:31 +0000

    mobile styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+// CHANGE THE TEXT COLOR WHEN IT IS CLICKED
 $(document).ready(function(){
   $('.core-nav-item').click(function(){
     $(this).addClass('core-nav-item-active');
     $('.core-nav-item').not(this).removeClass('core-nav-item-active');
   });
+
+
+// MOBILE MENU
+
+  let menu = $('#menu');
+  let drawer = $('.core-nav-items');
+
+  menu.on('click', function(){
+    drawer.toggleClass('open-menu');
+  });
+
 });
